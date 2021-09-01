2d87f365bf8a3d8cd2acd896ecc55a74605f87e8
HalisBay <eminbay_58@hotmail.com>
2021-09-01 11:29:42 +0300

    First Commit

---
+++ src/layouts/Categories.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Menu } from 'semantic-ui-react'
+
+export default function Categories() {
+    return (
+        <div>
+           <Menu pointing vertical>
+        <Menu.Item
+          name='alan1'
+          
+        />
+        <Menu.Item
+          name='alan2'
+         
+        />
+        <Menu.Item
+          name='alan3'
+          
+        />
+      </Menu>
+        </div>
+    )
+}
